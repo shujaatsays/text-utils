eaad7c7b6da789702e68245a25d7b1c2e42194db
Shujaat Ali <shujaat222@gmail.com>
2023-03-11 04:25:50 +0500

    Created react app and added Text utilities

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Navabar from './components/Navabar';
+import TextForm from './components/TextForm';
+import { useState } from 'react';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
+  const [mode, setmode] = useState('light')
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type) => {
+    setAlert({
+      message: message,
+      type: type
+    })
+
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500)
+  }
+
+  const toggleMode = () => {
+    mode === 'light' ? setmode('dark') : setmode('light')
+    showAlert(`${mode === 'light' ? 'dark' : 'light'} mode has been enabled`, "success")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navabar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Switch>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/">
+              <TextForm showAlert={showAlert} heading="Enter the text to analyze below" />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    let newText = text.toUpperCase()
+    setText(newText)
+    props.showAlert("Converted to upper case", 'success')
+  }
+
+  const handleLoClick = () => {
+    let newText = text.toLowerCase()
+    setText(newText)
+  }
+
+  const handleOnChange = (event) => {
+    setText(event.target.value)
+  }
+
+  const handleClear = () => {
+    let newText = ''
+    setText(newText)
+  }
+
+  const [text, setText] = useState('')
+  return (
+    <>
+      <div className='container'>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea className="form-control" placeholder='Enter Text Here' value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleClear}>Clear text</button>
+      </div>
+      <div className="container my-3">
+        <h1>Your text summary</h1>
+        <p>{text.split(' ').length} words and {text.length} characters </p>
+        <p>{0.008 * text.split(' ').length} minutes read</p>
+        <h2>Preview</h2>
+        <p>{text.length>0 ? text : 'Enter above to view Text'}</p>
+      </div>
+    </>
+  )
+}
